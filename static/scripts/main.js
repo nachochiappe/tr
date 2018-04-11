952eb130eb60938bb78513f562ae9a93d543f64c
Ignacio Chiappe <nacho.chiappe@gmail.com>
2018-04-11 20:45:54 -0300

    Se agrega búsqueda en tablas.

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,3 +1,15 @@
+$(document).ready(function () {
+    (function ($) {
+        $('#filter').keyup(function () {
+            var rex = new RegExp($(this).val(), 'i');
+            $('.searchable tr').hide();
+            $('.searchable tr').filter(function () {
+                return rex.test($(this).text());
+            }).show();
+        })
+    }(jQuery));
+});
+
 ///////////////////////////////
 // FUNCIONES DE MEDICAMENTOS //
 ///////////////////////////////
