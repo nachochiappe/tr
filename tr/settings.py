5058274e000207906a66159391f2367f45108f88
Ignacio Chiappe <nacho.chiappe@gmail.com>
2019-07-08 12:58:18 -0300

    Agrego STATIC_ROOT

--- tr/settings.py
+++ tr/settings.py
@@ -145,7 +145,7 @@ DATE_INPUT_FORMATS = ['%dd/%mm/%YYYY']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/templates/static/',
