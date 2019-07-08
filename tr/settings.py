9a6c17b382bf87c31f66e77fbdc9f25f433c2b3c
Ignacio Chiappe <nacho.chiappe@gmail.com>
2019-07-08 13:03:34 -0300

    Ejecuté collectstatic

--- tr/settings.py
+++ tr/settings.py
@@ -145,8 +145,8 @@ DATE_INPUT_FORMATS = ['%dd/%mm/%YYYY']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = '/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/templates/static/',
 ]
